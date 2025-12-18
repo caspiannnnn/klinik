3d4f753c0d5f2e0b64b1abfc4ba271ce6dbf110f
yassnath <andreasnath04@gmail.com>
2025-12-18 21:41:56 +0700

    Salam akal sehat

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,17 +27,13 @@ class User extends Authenticatable
         'jenis_kelamin',
         'no_hp',
         'nik',
-<<<<<<< HEAD
-=======
 
-        // ✅ FIX: kolom ini ada di migration & dipakai di register
         'alamat',
         'telepon',
 
-        // (opsional: kalau nanti kamu tambahkan di migration user)
+        // (opsional bila ada di migration nanti)
         // 'no_rm',
         // 'qr_path',
->>>>>>> af9d94b (feat: update clinic app (roles, API, swagger, views))
     ];
 
     protected $hidden = [
@@ -48,11 +44,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password'          => 'hashed',
-<<<<<<< HEAD
-        'tanggal_lahir'     => 'date',   // ✅ TAMBAH INI
-=======
         'tanggal_lahir'     => 'date',
->>>>>>> af9d94b (feat: update clinic app (roles, API, swagger, views))
     ];
 
     protected $appends = [
